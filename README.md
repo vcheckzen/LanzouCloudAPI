a0aee1f8ca2b5306ccf6f9bb93c323d65dd9107b
vcheckzen <vcheckzen@outlook.com>
2019-04-12 22:57:07 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # LanzouCloudAPI
-Redirect to download server when given lanzoucloud share link.
+
+## Introduction
+
+Redirect to download server when given lanzoucloud share link, implemented with Flask, python
+
+## Request Format
+
+If the share link has a password, then
+
+```html
+https://www.example.com?url=https://www.lanzous.com/i3clqna&type=down&pwd=b2ur
+```
+
+Or
+
+```html
+https://www.example.com?url=https://www.lanzous.com/i3qi3kf
+```
+
+## How to Use
+
+### Download the Code
+
+Assuming you've installed git, then
+
+```bash
+git clone https://github.com/vcheckzen/LanzouCloudAPI.git
+cd LanzouCloudAPI
+```
+
+### Install Requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run and Test
+
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import json
+import requests
+from flask import Flask, request, redirect, send_from_directory, jsonify
+
+
+app = Flask(__name__)
+port = int(os.getenv('PORT', '3000'))
+
+
+def get_para_simulate_pc(fid, pwd, host, headers):
+    response = requests.get(url=host + '/' + fid, headers=headers)
+    frame = str(re.findall(r"src=\"(.*)\" frameborder", response.text)[0])
+
+    response = requests.get(url=host + frame, headers=headers)
+    file_id = str(re.findall(r"\'(.{7})\'", response.text)[0])
+    t = str(re.findall(r"\'(.{10})\'", response.text)[0])
+    k = str(re.findall(r"\'(.{32})\'", response.text)[0])
+
+    return {
+        'action': 'down_process',
+        'file_id': file_id,
+        't': t,
+        'k': k,
+        'p': pwd,
+        'c': ''
+    }
+
+
+def get_link_simulate_phone(fid, host, headers):
+    response = requests.get(url=host + '/tp/' + fid, headers=headers)
+    urlp = str(re.findall(r"urlp = \'(.*)\'", response.text)[0])
+    para = str(re.findall(r"urlp \+ \'(.*)\'", response.text)[0])
+
+    return urlp +  para
+
+
+def get_para_simulate_phone(fid, pwd, host, headers):
+    response = requests.get(url=host + '/tp/' + fid, headers=headers)
+    para = str(re.findall(r"data : \'(.*)\'\+pwd", response.text)[0])
+    data = {}
+    for item in para.split('&'):
+        tmp = item.split('=')
+        data[tmp[0]] = tmp[1]
+    data['p'] = pwd
+
+    return data
+
+
+def get_direct_link(fid, pwd, type):
+    host = 'https://www.lanzous.com'
+    ua = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36', 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Mobile Safari/537.36']
+    headers = {
+        'User-Agent': ua[type],
+        'Referer': host
+    }
+    if type == 0:
+        data = get_para_simulate_pc(fid, pwd, host, headers)
+        response = requests.post(url=host + '/ajaxm.php',headers=headers, data=data)
+        result = json.loads(response.text)
+        link = result['dom'] + '/file/' + result['url']
+    else:
+        if pwd:
+            data = get_para_simulate_phone(fid, pwd, host, headers)
+            response = requests.post(url=host + '/ajaxm.php',headers=headers, data=data)
+            result = json.loads(response.text)
+            link = result['dom'] + '/file/' + result['url']
+        else:
+            link = get_link_simulate_phone(fid, host, headers)
+
+    return link
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def redirect_to_download_server():
+    if not re.match('.+/\?url=https:%2F%2Fwww\.lanzous\.com%2F[0-9a-z]{7,}.*', request.url):
+        return jsonify({'errno': '1', 'msg': 'not found'})
+
+    url = request.args.get('url')
+    fid = url.split('/')[3]
+    pwd = request.args.get('pwd')
+    
+    link = get_direct_link(fid, pwd, 1)
+    if link.find('baidupan.com') < 0:
+        link = get_direct_link(fid, pwd, 0)
+
+    return redirect(link)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
