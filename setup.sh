366d54f5d551050847881f0dc6fb0c51b309a69d
LOGI <18008498+vcheckzen@users.noreply.github.com>
2021-04-03 06:34:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+poetry config virtualenvs.in-project true
 poetry install
 cp lanzous.service /etc/systemd/system/
 systemctl daemon-reload
