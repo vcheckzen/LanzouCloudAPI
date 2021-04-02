27cb4c4292108fb3a4b9fac6a943c9e9164fdec0
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-04-03 06:03:41 +0800

    fix(run): environment issues

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 poetry install
 cp lanzous.service /etc/systemd/system/
 systemctl daemon-reload
