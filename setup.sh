7638f5a31e3e640af19ebbdc0fa18c9591f50e48
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-05-12 15:48:39 +0800

    fix: append the ip to hosts

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 poetry config virtualenvs.in-project true
 poetry install
+
+echo '47.91.203.9 pan.lanzou.com' >>/etc/hosts
 cp lanzous.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable --now lanzous
